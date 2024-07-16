a4848384ff85a95782c731a5751279c2eb1592fd
tusiimefortunateagnes <tfortunateagnes@gmail.com>
2024-07-16 17:11:39 +0300

    Login page can load

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+use Faker\Generator as Faker;
+use station\User;
+use station\Node;
+use station\Station;
+use station\Sensor;
+use station\NodeStatus;
+use station\NodeStatusConfiguration;
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(User::class, function (Faker $faker) {
+    static $password;
+
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('secret'),
+        'remember_token' => str_random(10),
+    ];
+});
+
+// $factory->define(station\Station::class, function (Faker $faker) {
+//     static $password;
+
+//     return [
+//         'station_name' => $faker->name,
+//         'station_location' => $faker->unique()->safeEmail,
+//         'longitude' => $faker->unique()->safeEmail,
+//         'latitude' => $faker->unique()->safeEmail,
+//         'station_number' => $faker->unique()->safeEmail,
+//         'location' => $faker->unique()->safeEmail,
+//         'city' => $faker->unique()->safeEmail,
+        
+//     ];
+// });
